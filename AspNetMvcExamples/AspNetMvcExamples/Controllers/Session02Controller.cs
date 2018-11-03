225ed0a4df81720fa5f395fd112efb834a0d7e1b
Ngo Thanh Tung <tungnt@softech.vn>
2018-11-03 19:29:08 +0700

    Update

--- AspNetMvcExamples/AspNetMvcExamples/Controllers/Session02Controller.cs
+++ AspNetMvcExamples/AspNetMvcExamples/Controllers/Session02Controller.cs
@@ -30,10 +30,25 @@ namespace AspNetMvcExamples.Controllers
 
         public ActionResult NewProductsPartial()
         {
-            var products500 = this.GetProducts().Where(x =>x.Price >= 500).ToList();
+            var newProducts = this.GetProducts().OrderByDescending(x => x.Id).ToList();
 
-            return PartialView("_NewProductsPartial", products500); // pass DataModel to PartialPage
+            return PartialView("_NewProductsPartial", newProducts); // pass DataModel to PartialPage
         }
+
+        public ActionResult TopProductsPartial()
+        {
+            var topProducts = this.GetProducts().OrderByDescending(x => x.Price).ToList();
+
+            return PartialView("_TopProductsPartial", topProducts); // pass DataModel to PartialPage
+        }
+
+        public ActionResult RelationProductsPartial(string category)
+        {
+            var releateProducts = this.GetProducts().Where(x => x.Category == category).ToList();
+
+            return PartialView("_RelationProductsPartial", releateProducts); // pass DataModel to PartialPage
+        }
+
 
         public ActionResult ProductDetailsPartial(int id)
         {
@@ -75,6 +90,7 @@ namespace AspNetMvcExamples.Controllers
             p1.Name = "iPhone 6";
             p1.Price = 500;
             p1.ImageUrl = "~/Uploads/Images/iphone6.jpg";
+            p1.Category = "Mobile Phone";
             products.Add(p1);
 
             // Product 2
@@ -83,6 +99,7 @@ namespace AspNetMvcExamples.Controllers
             p2.Name = "iPhone 6s";
             p2.Price = 600;
             p2.ImageUrl = "~/Uploads/Images/iphone6s.jpg";
+            p2.Category = "Mobile Phone";
             products.Add(p2);
 
             // Product 3
@@ -91,6 +108,7 @@ namespace AspNetMvcExamples.Controllers
             p3.Name = "iPhone 6s Plus";
             p3.Price = 800;
             p3.ImageUrl = "~/Uploads/Images/iphone6splus.jpg";
+            p3.Category = "Mobile Phone";
             products.Add(p3);
 
             // Product 4
@@ -99,6 +117,7 @@ namespace AspNetMvcExamples.Controllers
             p4.Name = "iPhone SE";
             p4.Price = 400;
             p4.ImageUrl = "~/Uploads/Images/iphoneSE.jpg";
+            p4.Category = "Other Phone";
             products.Add(p4);
 
             return products;
--- AspNetMvcExamples/AspNetMvcExamples/Models/ProductViewModels.cs
+++ AspNetMvcExamples/AspNetMvcExamples/Models/ProductViewModels.cs
@@ -9,6 +9,7 @@ namespace AspNetMvcExamples.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Category { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
     }
